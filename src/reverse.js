f6359225b36ba5fec4e608ea1640ea80a9c253b6
Sarah Rosales <srosales@ucvts.org>
2019-11-21 11:21:08 -0500

    exercise 2

--- src/reverse.js
+++ src/reverse.js
@@ -5,19 +5,22 @@ const MAX = Number.MAX_SAFE_INTEGER
 
 let integer = Number(readlineSync.question("\nPositive integer: "));
 let reverse = 0
-var b
+let reverseString = "";
 
 while (integer < MIN || integer > MAX || Number.isNaN(integer) || !Number.isInteger(integer)) {
   integer = Number(readlineSync.question("Positive integer: "));
 }
 
 while (integer > 0) {
-  reverse *= 10
-  reverse += integer % 10
-  integer -= integer % 10
-  integer /= 10
+  reverse = integer % 10;
+  integer = (integer-integer % 10) / 10;
+
+  if (integer == 0) {
+    reverseString = reverseString + reverse + ".";
+  } else if  (!integer == 0) {
+    reverseString = reverseString + reverse + ", ";
+  }
 
 }
 
-let str = reverse
-console.log(str.length);
+console.log("\n" + reverseString);
