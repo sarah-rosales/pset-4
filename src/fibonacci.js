aca56b7905730fe72f69d7bce091edd3eb135b0f
Sarah Rosales <srosales@ucvts.org>
2019-11-24 16:29:38 -0500

    exercise 6

---
+++ src/fibonacci.js
@@ -0,0 +1,25 @@
+const readlineSync = require("readline-sync");
+
+const MIN = 1;
+const MAX = 78;
+
+let a = 1, b = 0, n;
+
+console.log();
+
+let positive = Number(readlineSync.question("Positive integer: "));
+
+while (positive < MIN || positive > MAX || Number.isNaN(positive) || !Number.isInteger(positive)) {
+  positive = Number(readlineSync.question("Positive integer: "));
+}
+
+while (positive >= MIN) {
+  n = a;
+  a = a + b;
+  b = n;
+  positive--;
+}
+
+b = b.toLocaleString({maximumFractionDigits: 2, minimumFractionDigits: 2});
+
+console.log("\n" + b);
