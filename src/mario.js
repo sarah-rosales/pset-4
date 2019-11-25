4cf1d9ef9034ab34ab8d0a0520d312c09f6db9b3
Sarah Rosales <srosales@ucvts.org>
2019-11-24 19:46:45 -0500

    exercise 8

---
+++ src/mario.js
@@ -0,0 +1,26 @@
+const readlineSync = require("readline-sync");
+
+const MIN = 1
+const MAX = 24
+
+console.log();
+
+let height = Number(readlineSync.question("Height: "));
+let row = height
+let pyramid;
+
+while (height < MIN || height > MAX || Number.isNaN(height) || !Number.isInteger(height)) {
+  height = Number(readlineSync.question("Height: "));
+}
+
+console.log();
+while (row > 0) {
+  pyramid = ""
+  for (let i = 0; i <= height; i++) {
+      pyramid += "#"
+  } for (i = 0; i < row - 1; i++) {
+      pyramid = pyramid.replace(pyramid.charAt(i), " ");
+  }
+    console.log( pyramid);
+    row -= 1
+  }
